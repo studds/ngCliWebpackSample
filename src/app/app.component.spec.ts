64b5f559be8d2f2f122deabbece47f4d10b0ed25
Daniel Studds <daniel.studds@vicsuper.com.au>
2017-10-24 08:19:24 +1100

    Adding dependency on angular2-moment

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,15 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { MomentModule } from 'angular2-moment';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [
+        MomentModule
+      ]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
